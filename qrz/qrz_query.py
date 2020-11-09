bec70a2c0a53e6cbd08692717fc84a02ce848ced
Kenji Rikitake <kenji.rikitake@acm.org>
2020-11-09 14:43:31 +0900

    QRZ(): remove code for disabling SSL verification
    Rationale:
    
    In HTTPS access for qrz.com, the host should always be verified by the TLS/SSL certificate.
    
    Implementation details:
    
    * Python `requests` package uses `urllib3`
    * In many cases using `urllib3` include importing `certifi`
    * By importing certifi, you're ready to verify the TLS/SSL certificate
    
    See <https://urllib3.readthedocs.io/en/latest/user-guide.html#certificate-verification> for the further details.

--- qrz/qrz_query.py
+++ qrz/qrz_query.py
@@ -41,7 +41,6 @@ class QRZ(object):
 
         url = '''https://xmldata.qrz.com/xml/current/?username={0}&password={1}'''.format(username, password)
         self._session = requests.Session()
-        self._session.verify = bool(os.getenv('SSL_VERIFY', False))
         r = self._session.get(url)
         if r.status_code == 200:
             raw_session = xmltodict.parse(r.content)
